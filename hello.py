72a6ee0988811200b80e8f6a3ac02117f255b038
Nagesh <yashchavan192001@gmail.com>
2024-05-15 00:28:59 +0530

    first python code

---
+++ hello.py
@@ -0,0 +1,11 @@
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+        
+n = int(input("Enter the value: "))
+
+
+for i in range(1,n):
+    print(fibo(i), end="")
